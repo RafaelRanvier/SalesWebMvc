4e50975aa56d770616422a04e98452024aef9258
RafaelRanvier <rafaelranvier@bol.com.br>
2023-03-12 00:55:59 -0300

    SellerService e FindAll sincrono

---
+++ SalesWebMvc/Migrations/20230312034434_ChangeToBaseSalary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class ChangeToBaseSalary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Salary",
+                table: "Seller",
+                newName: "BaseSalary");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BaseSalary",
+                table: "Seller",
+                newName: "Salary");
+        }
+    }
+}
